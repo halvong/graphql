a4852590d4508a20d3a904b30e294e50617a3e6a
Hal Vong <halvong@yahoo.com>
2020-06-05 22:30:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Migrating from REST to GraphQL, Linkedin 
+6/5/20, Fri
+
+#url
+https://www.linkedin.com/learning/migrating-from-rest-to-graphql/setting-up-the-initial-resolvers?pathUrn=urn%3Ali%3AlyndaLearningPath%3A5a7e16cf498eb83924bbb826
+
+
+brew services start mongodb-community
+brew services stop mongodb-community
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+const app = express();
+const PORT = 8080;
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.send(`GraphQL is running!`)
+);
+
+app.use('/graphql',graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(PORT, () =>
+    console.log(`your server is running on localhost:${PORT}/graphql`)
+);
